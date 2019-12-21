76698ba504c27ab754c62126a5f0f5cfe32afe4b
Tania <taniameoow@gmail.com>
2019-12-21 20:35:21 +0200

    add homework

--- task2.js
+++ task2.js
@@ -1,30 +1,29 @@
 const users = [
-    { name: 'Mango', active: true },
-    { name: 'Poly', active: false },
-    { name: 'Ajax', active: true },
-    { name: 'Lux', active: false },
-  ];
-  
-  const toggleUserState = (allUsers, userName) => {
-      return new Promise(resolve=>{
-        const updatedUsers = allUsers.map(user =>
-            user.name === userName ? { ...user, active: !user.active } : user,
-          );
-          resolve(updatedUsers);
-      })
+  { name: "Mango", active: true },
+  { name: "Poly", active: false },
+  { name: "Ajax", active: true },
+  { name: "Lux", active: false }
+];
 
-  };
-  
-  const logger = updatedUsers => console.table(updatedUsers);
-  
-  /*
-   * Сейчас работает так
-   */
+const toggleUserState = (allUsers, userName) => {
+  return new Promise(resolve => {
+    const updatedUsers = allUsers.map(user =>
+      user.name === userName ? { ...user, active: !user.active } : user
+    );
+    resolve(updatedUsers);
+  });
+};
+
+const logger = updatedUsers => console.table(updatedUsers);
+
+/*
+ * Сейчас работает так
+ */
 //   toggleUserState(users, 'Mango', logger);
 //   toggleUserState(users, 'Lux', logger);
-  
-  /*
-   * Должно работать так
-   */
-  toggleUserState(users, 'Mango').then(logger);
-  toggleUserState(users, 'Lux').then(logger);
+
+/*
+ * Должно работать так
+ */
+toggleUserState(users, "Mango").then(logger);
+toggleUserState(users, "Lux").then(logger);
